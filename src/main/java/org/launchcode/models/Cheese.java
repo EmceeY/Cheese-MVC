d8f820313350d7613b5c9075ff68084e68d1abe4
MCY <mcoreyyares@gmail.com>
2017-03-13 16:01:53 -0500

    created a Cheese class

---
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -0,0 +1,26 @@
+package org.launchcode.models;
+
+/**
+ * Created by mcoreyyares on 3/13/17.
+ */
+public class Cheese {
+    private String name;
+    private String description;
+
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String aName){
+        name =  aName;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String aDescription){
+        description = aDescription;
+    }
+}
